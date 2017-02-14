aea2012cabbcc99f09559ac33d71d14a3f45a116
User1561 <william.a.solomon1@gmail.com>
2017-02-14 10:01:04 +0000

    First attempt at building the calculator
    An attempt at building the calculator.

---
+++ File.cpp
@@ -0,0 +1,46 @@
+#include "stdafx.h" 
+#include <stdio.h>
+#include "stdafx.h"
+#include <iosteam>
+using namespace std;
+{
+
+int main()
+    cout <<"Please enter the first number";
+
+int Number1;
+cin >> Number1;
+
+cout >> "Please enter a function";
+
+char Function;
+cin >> Function;
+
+cout << "Please enter another number";
+
+int Number2;
+cin >> Number2;
+
+int Result;
+if(function== "+");
+{
+Result = Number1 + Number2;
+}
+
+else if (function =="-");
+{
+    Result = Number1 - Number2;
+}
+else if (function=="*");
+{
+Result = Number1 * Number2;
+}
+else if (function=="/");
+{
+Result = Number1 / Number2;
+}
+
+cout << Result;
+system ("pause")
+return 0;
+}
